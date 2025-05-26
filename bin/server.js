6fb6877136871d360af23d7ac4457a86af29b1b9
Agatha Dantas <workdantas@gmail.com>
2025-05-26 18:01:02 -0300

    ♻ refactor: reorganiza estrutura do projeto em src e bin separando server e app

--- bin/server.js
+++ bin/server.js
@@ -1,33 +1,23 @@
 //colocando o javaScript para ficar mais criterioso
 'use strict';
 
+const app = require('../src/app');
 const http = require('http');
-const express = require('express');
 const debug = require('debug')('nodestr:server');
 
-//criando a aplicação
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port) //setando a porta
 
 const server = http.createServer(app);//criando o servidor
-const router = express.Router(); 
-
-//configurando a primeira rota
-let route = router.get('/', (req, res, next) => { 
-    res.status(200).send({
-        title: 'Node Store Api',
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
 
 server.listen(port); //aqui diz para o servidor ficar ouvindo nessa porta
-server.on('error',onError);
+server.on('error', onError);
+server.on('listening ',onListening);
 
 console.log('Api rodando na porta ' + port);
 
 //função para validar se a porta está disponível
+/*na função normalizePort, recebe como parâmetro um valor, converte o valor  um número inteiro, na primeira condição, se o valor não for um número ele retorna 10, se a port for maior ou igual a zero retorna a port */
 function normalizePort(val) {
     const port = parseInt(val, 10);
     if (isNaN(port)) {
@@ -40,11 +30,6 @@ function normalizePort(val) {
 
     return false;
 }
-
-/*na função normalizePort, recebe como parâmetro um valor, converte o valor  um número inteiro, na primeira condição, se o valor não for um número ele retorna 10, se a port for maior ou igual a zero retorna a port */
-
-
-
 
 // Função para tratar erros ao iniciar o servidor.
 // Verifica se o erro está relacionado ao 'listen' e trata casos comuns:
@@ -73,4 +58,13 @@ function onError(error) {
             throw error;
         
     }
+}
+
+// indicar que o servidor iniciou com sucesso, obtém o endereço/porta e imprime essa informação no console via debug()
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listing on ' + bind);
 }
