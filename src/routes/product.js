42b434efd097ffc62bcc07cae15c0a181d2a4f83
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-06-26 17:44:59 -0300

    ✨feat: add route to search product by slug

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,9 +4,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
+router.get('/',controller.get);
+router.get('/:slug',controller.getBySlug);
 router.post('/',controller.post);
 router.put('/',controller.put);
 router.delete('/',controller.delete);
-router.get('/',controller.get);
 
 module.exports = router;
